226d3634d3f8e1369e7ab682489a7b263fa97de0
Andrey Agarkov <business.yavorprod@gmail.com>
2023-12-01 15:07:08 +0300

    add gunicorn & nginx to docker build

--- docker/backend/entrypoint.dev.sh
+++ docker/backend/entrypoint.dev.sh
@@ -13,6 +13,6 @@ do
 done
 
 
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
-python manage.py runserver 0.0.0.0:8000
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
