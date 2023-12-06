7627d9f026efc61838326aed55bdd43e869a4fb6
Andrey Agarkov <agarkov@peterpartner.net>
2023-12-06 19:21:01 +0300

    add profile routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,4 +14,16 @@ urlpatterns = [
     ),
     path("auth/send_code", views.AuthSendCodeView.as_view(), name="auth-code"),
     path("auth/", views.AuthView.as_view(), name="auth"),
+    path(
+        "profile/",
+        views.ProfileUpdateViewSet.as_view(
+            {"put": "update", "patch": "partial_update"}
+        ),
+        name="my-profile",
+    ),
+    path(
+        "profile/<int:pk>/",
+        views.ProfileDetailViewSet.as_view({"get": "retrieve", "delete": "destroy"}),
+        name="user-profile",
+    ),
 ]
