2865f3df5a2cde0077a152b269033650d4479348
Andrey Agarkov <business.yavorprod@gmail.com>
2023-12-18 19:25:39 +0300

    add event media get method

--- backend/api/views/media.py
+++ backend/api/views/media.py
@@ -2,14 +2,17 @@ from rest_framework_bulk.mixins import BulkCreateModelMixin
 from rest_framework.generics import ListAPIView
 
 from api.permissions import MailIsConfirmed
-from api.serializers import EventMediaBulkCreateSerializer
+from api.serializers import EventMediaBulkCreateSerializer, EventMediaListSerializer
 from api.models import EventMedia
 from api.services import get_event_object
 
 
 class EventMediaListCreateView(BulkCreateModelMixin, ListAPIView):
     permission_classes = [MailIsConfirmed]
-    serializer_class = EventMediaBulkCreateSerializer
+    serializer_class = {
+        "GET": EventMediaListSerializer,
+        "POST": EventMediaBulkCreateSerializer,
+    }
     queryset = EventMedia.objects.all()
 
     def get_queryset(self):
@@ -20,6 +23,9 @@ class EventMediaListCreateView(BulkCreateModelMixin, ListAPIView):
         context = super().get_serializer_context()
         context["event"] = get_event_object(self.kwargs["pk"])
         return context
+
+    def get_serializer_class(self):
+        return self.serializer_class[self.request.method]
 
     def post(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
