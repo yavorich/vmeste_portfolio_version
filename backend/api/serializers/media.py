ad673fa06a595a6f7b474041e17ff707c95fc250
Andrey Agarkov <business.yavorprod@gmail.com>
2023-12-18 18:05:12 +0300

    events media base

---
+++ backend/api/serializers/media.py
@@ -0,0 +1,109 @@
+import base64
+import filetype
+import binascii
+
+from rest_framework_bulk.serializers import BulkListSerializer, BulkSerializerMixin
+from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ValidationError
+from django.core.files.base import ContentFile
+from django.utils.translation import gettext_lazy as _
+from drf_extra_fields.fields import Base64FileField, Base64FieldMixin
+from rest_framework.settings import api_settings
+
+from api.models import EventMedia
+
+
+def validate_file_size(file):
+    """Установить ограничение для загружаемых файлов"""
+    if not file:
+        return
+    max_size = 50 * 1024 * 1024
+    if file.size > max_size:
+        raise ValidationError("Размер файла не должен превышать 50 МБ.")
+
+
+class UrlWithoutDomainMixin:
+    def to_representation(self, value):
+        if not value:
+            return None
+
+        use_url = getattr(self, "use_url", api_settings.UPLOADED_FILES_USE_URL)
+        if use_url:
+            try:
+                url = value.url
+            except AttributeError:
+                return None
+
+            return url
+
+        return value.name
+
+
+class CustomFileField(UrlWithoutDomainMixin, Base64FileField):
+    INVALID_FILE_MESSAGE = "Некорректный файл"
+
+    def to_internal_value(self, base64_data):
+        # Check if this is a base64 string
+        if base64_data in self.EMPTY_VALUES:
+            return super(Base64FieldMixin, self).to_internal_value(None)
+
+        if isinstance(base64_data, str):
+            # Strip base64 header.
+            if ";base64," in base64_data:
+                header, base64_data = base64_data.split(";base64,")
+
+            # Try to decode the file. Return validation error if it fails.
+            try:
+                decoded_file = base64.b64decode(base64_data)
+            except (TypeError, binascii.Error, ValueError):
+                raise ValidationError(self.INVALID_FILE_MESSAGE)
+            # Generate file name:
+            file_name = self.get_file_name(decoded_file)
+            # Get the file name extension:
+            file_extension = self.get_file_extension(file_name, decoded_file)
+
+            complete_file_name = file_name + "." + file_extension
+            data = ContentFile(decoded_file, name=complete_file_name)
+            return super(Base64FieldMixin, self).to_internal_value(data)
+        raise ValidationError(
+            _(
+                "Invalid type. This is not an base64 string: {}".format(
+                    type(base64_data)
+                )
+            )
+        )
+
+    def get_file_extension(self, filename, decoded_file):
+        try:
+            kind = filetype.guess(decoded_file)
+            return kind.extension
+        except AttributeError:
+            raise ValidationError(self.INVALID_FILE_MESSAGE)
+
+
+class EventMediaBulkCreateSerializer(BulkSerializerMixin, ModelSerializer):
+    file = CustomFileField(validators=[validate_file_size])
+
+    class Meta:
+        list_serializer_class = BulkListSerializer
+        model = EventMedia
+        fields = [
+            "file_type",
+            "weight",
+            "duration",
+            "file",
+            "file_name",
+            "uploaded_at",
+        ]
+
+    def create(self, validated_data):
+        if validated_data["media_type"] == EventMedia.MediaType.VIDEO:
+            if not validated_data.get("duration"):
+                raise ValidationError(
+                    "File of type 'video' must have 'duration' parameter"
+                )
+        if validated_data["media_type"] == EventMedia.MediaType.PHOTO:
+            if "duration" in validated_data:
+                validated_data.pop("duration")
+
+        return super().create(validated_data)
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # from api.urls import urlpatterns as api_urlpatterns
 # from chat.urls import urlpatterns as chat_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path(
-        'api/v1/', include(
+        "api/v1/",
+        include(
             [
                 path("", include("api.urls", namespace="api")),
                 path("", include("chat.urls", namespace="chat")),
             ]
-        )
+        ),
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
