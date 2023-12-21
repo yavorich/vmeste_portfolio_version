95287a124b8fc0d4442ec94672fe51579193d578
Andrey Agarkov <business.yavorprod@gmail.com>
2023-12-21 10:43:03 +0300

    add UserAdmin & related admin models

---
+++ backend/api/admin/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa: F401
+
+from .user import admin
+from .category import admin
+from .event import admin
---
+++ backend/core/utils/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa: F401
+
+from .validate_file_size import validate_file_size
+from .parse_json_bool import parse_json_bool
+from .convert_file_to_base64 import convert_file_to_base64
