7ba7d5721fbc0dff6a6400486e2f0aff86b5680a
Andrey Agarkov <business.yavorprod@gmail.com>
2023-12-29 14:54:06 +0300

    fix base64 converting

--- backend/core/utils/convert_file_to_base64.py
+++ backend/core/utils/convert_file_to_base64.py
@@ -4,9 +4,8 @@ import base64
 def convert_file_to_base64(file):
     try:
         path = file.path
-        extension = path.split(".")[-1]
         with open(path, "rb") as f:
             data = f.read()
-            return "data:image/%s;base64,%s" % (extension, base64.b64encode(data))
+            return base64.b64encode(data).decode()
     except (ValueError, FileNotFoundError):
         return None
