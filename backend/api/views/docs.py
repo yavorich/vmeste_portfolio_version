98189435775a93525fb6a9ca25758d79117beab4
Andrey Agarkov <agarkov@peterpartner.net>
2023-12-29 14:55:23 +0300

    remove 400 response in agreement

--- backend/api/views/docs.py
+++ backend/api/views/docs.py
@@ -29,10 +29,9 @@ class DocsViewSet(ListModelMixin, GenericViewSet):
         user = request.user
         obj = self.get_object()
         if obj.name == Docs.Name.AGREEMENT:  # с rules пока ничего не делаем
-            if user.agreement_applied_at:
-                raise ValidationError("Agreement was already applied")
-            user.agreement_applied_at = now()
-            user.save()
+            if not user.agreement_applied_at:
+                user.agreement_applied_at = now()
+                user.save()
         return Response(f"{obj.name} applied.", status=HTTP_200_OK)
 
     def list(self, request, *args, **kwargs):
