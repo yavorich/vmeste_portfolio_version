05cbec9f4469ed71b0f91186ce61d0ea8e127160
Andrey Agarkov <business.yavorprod@gmail.com>
2024-01-10 10:21:21 +0300

    rework socket

--- backend/chat/signals.py
+++ backend/chat/signals.py
@@ -1,9 +1,13 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 
-from api.models import EventParticipant
+from api.models import Event, EventParticipant
 from chat.models import Message
-from chat.utils import send_ws_message
+from chat.utils import (
+    send_ws_message,
+    add_user_to_group,
+    remove_user_from_group,
+)
 from chat.serializers import MessageSerializer
 
 
@@ -26,9 +30,22 @@ def send_info_message(instance: EventParticipant, join: bool):
 @receiver(post_save, sender=EventParticipant)
 def send_join_message(sender, instance: EventParticipant, created: bool, **kwargs):
     if created:
+        add_user_to_group(instance.event)
         send_info_message(instance, join=True)
 
 
 @receiver(post_delete, sender=EventParticipant)
 def send_leave_message(sender, instance: EventParticipant, **kwargs):
+    remove_user_from_group(instance.event)
     send_info_message(instance, join=False)
+
+
+@receiver(post_save, sender=Event)
+def create_chat_group(sender, instance: Event, created: bool, **kwargs):
+    if created:
+        add_user_to_group(instance)
+
+
+@receiver(post_delete, sender=Event)
+def delete_chat_group(sender, instance: Event, **kwargs):
+    remove_user_from_group(instance)
