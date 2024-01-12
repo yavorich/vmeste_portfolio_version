2c25129eb0d3d44d5014a91f4dd5037d86427603
Andrey Agarkov <business.yavorprod@gmail.com>
2024-01-12 13:01:52 +0300

    increase token lifetime

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -168,7 +168,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "api.User"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
