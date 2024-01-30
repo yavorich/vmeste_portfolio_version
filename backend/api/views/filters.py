2a2ee94b9a33e384db47432cefba37c933cd2281
Andrey Agarkov <agarkov@peterpartner.net>
2024-01-30 09:17:28 +0300

    change fast filters permissions

--- backend/api/views/filters.py
+++ backend/api/views/filters.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from api.models import EventFastFilter
 from api.serializers import EventFastFilterSerializer
@@ -7,5 +7,5 @@ from api.serializers import EventFastFilterSerializer
 
 class EventFastFiltersListView(ListAPIView):
     queryset = EventFastFilter.objects.filter(is_active=True)
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     serializer_class = EventFastFilterSerializer
