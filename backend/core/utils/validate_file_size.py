74108a66d3eb5eafe4d8ab44c9e6a2c2d84c6989
Andrey Agarkov <business.yavorprod@gmail.com>
2024-03-07 18:07:44 +0300

    increase all files size limits

--- backend/core/utils/validate_file_size.py
+++ backend/core/utils/validate_file_size.py
@@ -5,6 +5,6 @@ def validate_file_size(file):
     """Установить ограничение для загружаемых файлов"""
     if not file:
         return
-    max_size = 50 * 1024 * 1024
+    max_size = 200 * 1024 * 1024
     if file.size > max_size:
         raise ValidationError({"error": "Размер файла не должен превышать 50 МБ."})
