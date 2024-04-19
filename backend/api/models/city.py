0976ae26189d787c42e4ea490d039921dbbd9925
Andrey Agarkov <business.yavorprod@gmail.com>
2024-04-19 17:01:30 +0300

    upd - city ordering

--- backend/api/models/city.py
+++ backend/api/models/city.py
@@ -13,6 +13,7 @@ class City(models.Model):
     class Meta:
         verbose_name = "Город"
         verbose_name_plural = "Города"
+        ordering = ["name"]
 
     def __str__(self) -> str:
         return self.name
