ea91f4ec6e579d6cce9f175a9639ac29f9282aa1
Andrey Agarkov <business.yavorprod@gmail.com>
2024-04-19 15:06:30 +0300

    fix - previous commit

--- backend/api/views/token.py
+++ backend/api/views/token.py
@@ -1,6 +1,9 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+from rest_framework import status
+
 from api.serializers import UserTokenObtainPairSerializer, UserTokenRefreshSerializer
 
 
@@ -12,7 +15,11 @@ class UserTokenRefreshView(TokenRefreshView):
     serializer_class = UserTokenRefreshSerializer
 
     def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
         try:
-            return super().post(request, *args, **kwargs)
-        except InvalidToken:
+            serializer.is_valid(raise_exception=True)
+        except TokenError:
             raise PermissionDenied(detail="Refresh token invalid or expired", code=403)
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
