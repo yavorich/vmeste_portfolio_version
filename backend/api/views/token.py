ab7f0a5acae840d113700f477db8875f7510b9d1
Andrey Agarkov <business.yavorprod@gmail.com>
2024-04-19 15:02:02 +0300

    upd - change invalid refresh token status code (401 -> 403)

--- backend/api/views/token.py
+++ backend/api/views/token.py
@@ -1,5 +1,6 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework.exceptions import PermissionDenied
 from api.serializers import UserTokenObtainPairSerializer, UserTokenRefreshSerializer
 
 
@@ -9,3 +10,9 @@ class UserTokenObtainPairView(TokenObtainPairView):
 
 class UserTokenRefreshView(TokenRefreshView):
     serializer_class = UserTokenRefreshSerializer
+
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except InvalidToken:
+            raise PermissionDenied(detail="Refresh token invalid or expired", code=403)
