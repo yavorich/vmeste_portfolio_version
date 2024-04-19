5189580c2fa3fc294134e03d21c0965b6d2bac28
Andrey Agarkov <business.yavorprod@gmail.com>
2024-04-19 17:34:12 +0300

    tmp - refresh 15 sec lifetime

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -170,7 +170,7 @@ AUTH_USER_MODEL = "api.User"
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
+    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=15),
 }
 
 AUTHENTICATION_BACKENDS = [
