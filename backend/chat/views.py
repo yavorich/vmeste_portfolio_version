e7fcab7a49bfacdd6b3aaddd042c774b9cfdf843
Andrey Agarkov <business.yavorprod@gmail.com>
2024-04-22 17:43:24 +0300

    upd - disable default pagination for chats

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -49,10 +49,18 @@ class ChatListView(ListAPIView):
         return context
 
     def list(self, request, *args, **kwargs):
-        chats = super().list(request, *args, **kwargs).data
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None and "page" in request.query_params:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
         unread_notify = request.user.notifications.filter(read=False).count()
         return Response(
-            data={"unread_notify": unread_notify, "chats": chats}, status=HTTP_200_OK
+            data={"unread_notify": unread_notify, "chats": serializer.data},
+            status=HTTP_200_OK,
         )
 
 
