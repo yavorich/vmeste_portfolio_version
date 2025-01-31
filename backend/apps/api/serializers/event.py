a11c931aa621e734cb134fe674bc906602aaddd1
Leonid <titov281@yandex.ru>
2025-01-31 13:36:23 +0500

    fix - disable event sign error

--- backend/apps/api/serializers/event.py
+++ backend/apps/api/serializers/event.py
@@ -530,8 +530,8 @@ class EventSignSerializer(ModelSerializer):
 
     def update(self, instance: Event, validated_data):
         user = self.context["user"]
-        if not instance.is_valid_sign_and_edit_time():
-            raise ValidationError({"error": "Время вступления на событие истекло."})
+        # if not instance.is_valid_sign_and_edit_time():
+        #     raise ValidationError({"error": "Время вступления на событие истекло."})
 
         if instance.get_free_places(gender=user.gender) == 0:
             raise ValidationError(
