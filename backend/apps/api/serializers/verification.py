f86c80d05d237469b6bd361632d9299a39e07fdf
andre <business.yavorprod@gmail.com>
2025-07-22 16:16:33 +0300

    fix - verification routes

--- backend/apps/api/serializers/verification.py
+++ backend/apps/api/serializers/verification.py
@@ -1,13 +1,20 @@
 from rest_framework.serializers import ModelSerializer
+from django.db import IntegrityError
 
 from apps.api.models import Verification
 from core.file_serializers.fields.file_field import NameSizeFileField
 
 
 class VerificationSerializer(ModelSerializer):
-    file = NameSizeFileField()
+    document_file = NameSizeFileField()
 
     class Meta:
         model = Verification
-        fields = ("file", "confirmed")
+        fields = ("document_file", "confirmed")
         read_only_fields = ("confirmed",)
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            return self.context["request"].user.verification
--- backend/apps/api/views/verification.py
+++ backend/apps/api/views/verification.py
@@ -19,6 +19,15 @@ class VerificationView(
             return self.request.user.verification
         except ObjectDoesNotExist:
             raise NotFound
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
